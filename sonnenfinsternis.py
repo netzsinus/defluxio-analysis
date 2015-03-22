35dca46ff3a9bd1daf8a0883c24e1fcb4bd9b51d
Mathias Dalheimer <md@gonium.net>
2015-03-22 23:02:56 +0100

    vergleich der verteilungen implementiert
    Former-commit-id: bbf3f2bd6f63eb48effc349df9c1ee28dc73267d

--- sonnenfinsternis.py
+++ sonnenfinsternis.py
@@ -1,11 +1,14 @@
 # vim:fileencoding=utf-8
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib.cm as cm
 import matplotlib as mpl
 import numpy as np
 import pandas as pd
 import datetime as dt
 import freqanalysis.datatools as datatool
+import freqanalysis.ecdf as ecdf
+from scipy import stats
 import argparse
 
 cmd_parser = argparse.ArgumentParser()
@@ -28,6 +31,7 @@ def add_freq_momentum(dataset):
 with pd.get_store(args.datafile) as store:
   eclipsedata = store['eclipsedata']
   fridaydata = store['fridaydata']
+
   print "Drawing solar eclipse frequency overview"
   f, ax = plt.subplots()
   lower_freq_limit = 49.95
@@ -59,7 +63,8 @@ with pd.get_store(args.datafile) as store:
   ax[0].set_xlabel("Zeit [Sekunden seit Mitternacht UTC]")
   ax[0].set_ylabel("Gradient [MW/min]")
   ax[0].set_ylim((lower_momentum_limit, upper_momentum_limit))
-  #ax[0].set_xlim((np.min(eclipsedata.time), np.max(eclipsedata.time)))
+  ax[0].set_xlim((np.min(eclipse_momentum_df.s_since_midnight.astype(int)), 
+    np.max(eclipse_momentum_df.s_since_midnight.astype(int))))
   ax[0].plot(eclipse_momentum_df.s_since_midnight.astype(int),
       eclipse_momentum_df.momentum, 'b.', label="Momentum")
   #ax.legend()
@@ -72,7 +77,8 @@ with pd.get_store(args.datafile) as store:
   ax[1].set_xlabel("Zeit [Sekunden seit Mitternacht UTC]")
   ax[1].set_ylabel("Gradient [MW/min]")
   ax[1].set_ylim((lower_momentum_limit, upper_momentum_limit))
-  #ax[1].set_xlim((np.min(eclipsedata.time), np.max(eclipsedata.time)))
+  ax[1].set_xlim((np.min(eclipse_momentum_df.s_since_midnight.astype(int)), 
+    np.max(eclipse_momentum_df.s_since_midnight.astype(int))))
   ax[1].plot(friday_momentum_df.s_since_midnight.astype(int),
       friday_momentum_df.momentum, 'b.', label="Momentum")
   #ax.legend()
@@ -84,4 +90,47 @@ with pd.get_store(args.datafile) as store:
 
   f.suptitle("Sonnenfinsternis am 20.03.2015 - Leistungsgradienten")
   f.autofmt_xdate()
-  plt.savefig("images/sonnenfinsternis-momentum.png")#, bbox_inches='tight')
+  plt.savefig("images/sonnenfinsternis-gradienten.png")#, bbox_inches='tight')
+
+  plt.clf()
+  lower_freq_limit = 49.95
+  upper_freq_limit = 50.05
+  plt.xlabel("Zeit [UTC]")
+  plt.ylabel("Gradient [MW/min]")
+  plt.hexbin(friday_momentum_df.s_since_midnight.astype(int),
+      friday_momentum_df.momentum, gridsize=80, cmap=cm.Reds)
+  cp=plt.colorbar()
+  cp.set_label(u"Häufigkeit")
+  #plt.plot(eclipse_momentum_df.s_since_midnight.astype(int),
+  #    eclipse_momentum_df.momentum, 'r-', label="Momentum")
+
+  #hfmt = dates.DateFormatter('%H:%M')
+  #ax.xaxis.set_major_formatter(hfmt)
+#  y_formatter = mpl.ticker.ScalarFormatter(useOffset=False)
+#  ax.yaxis.set_major_formatter(y_formatter)
+#  ax.grid(True)
+
+  f.suptitle("Dichte der Leistungsgradienten")
+  f.autofmt_xdate()
+  plt.savefig("images/sonnenfinsternis-dichte-gradienten.png")#, bbox_inches='tight')
+
+  plt.clf()
+  friday_series, friday_vals = ecdf.get_ecdf(friday_momentum_df.momentum)
+  ecdf.plot_ecdf_curve(friday_series, friday_vals, color="b", label="Typischer Freitag")
+  eclipse_series, eclipse_vals = ecdf.get_ecdf(eclipse_momentum_df.momentum)
+  ecdf.plot_ecdf_curve(eclipse_series, eclipse_vals, color="r", label="Sonnenfinsternis")
+  print "Mittelwert alle Freitage: %f" % np.median(friday_momentum_df.momentum)
+  print "Mittelwert Sonnenfinsternis: %f" % np.median(eclipse_momentum_df.momentum)
+  # http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.levene.html#scipy.stats.levene
+  W, p_val = stats.levene(friday_momentum_df.momentum,
+      eclipse_momentum_df.momentum, center='median')
+  print ("Levenes Test auf Gleichheit der Varianz: P=%s (gleiche Varianz für p<=0.05)" % p_val)
+
+
+  W, p_val = stats.fligner(friday_momentum_df.momentum, eclipse_momentum_df.momentum)
+  print "Fliegners Test auf Gleichheit der Varianz: P=%s" % p_val
+
+  f.suptitle("ECDF der Leistungsgradienten: Ungleiche Varianzen (Levene, p=%f)" % p_val)
+  plt.savefig("images/sonnenfinsternis-ecdf-gradienten.png")#, bbox_inches='tight')
+
+ 
